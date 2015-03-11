d7a4000013595496b0a76926be7631509c03a08a
heyxiaoling <1186608119@qq.com>
2015-03-11 09:01:11 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,4 +1,38 @@
 (function(window,undefine){
+    var client = function () {//检测各种版本
+        var engine = {
+            //呈现引擎
+            ie: 0,
+            gecko: 0,
+            webkit: 0,
+            khtml: 0,
+            opera: 0,
+            //具体版本号
+            ver: null
+        };
+        var browser = {
+            //浏览器
+            ie: 0,
+            firefox: 0,
+            konq: 0,
+            opera: 0,
+            chrome: 0,
+            safari: 0,
+            //具体版本号
+            ver: null
+        };
+        var system = {
+            win: false,
+            mac: false,
+            x11: false
+        };
+        //在此检测呈现引擎、平台设备
+        return {
+            engine: engine,
+            browser: browser,
+            system: system
+        };
+    }();
     var EventUtil = {
         addHandler: function(element,type,handler){
           if(element.addEventListener){
@@ -67,5 +101,5 @@
         }
     }
 
- return window.EventUtil=EventUtil;
+ window.EventUtil=EventUtil;
 })(window)
